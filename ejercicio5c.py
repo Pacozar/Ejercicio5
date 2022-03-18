edad2e9ba82cf7c2dbae50aff9b521d4a93d9faa
Francisco Javier Cozar Moreno <pakeraskeras@gmail.com>
2022-03-18 11:36:47 +0100

    a

---
+++ ejercicio5c.py
@@ -0,0 +1,15 @@
+def bisiesto():
+    while True:
+        try:
+            numero = int(input('Escribe un año: \n'))
+            break
+        except:
+            print('Escribe un año valido')
+
+    if numero % 4 == 0:
+        print(numero, 'es un año bisiesto')
+    elif numero % 4 != 0:
+        print(numero, 'no es un año bisiesto')
+
+    bisiesto()
+bisiesto()
